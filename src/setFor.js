61ec4cc3205e279aea50736236eae394481a867e
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2019-03-08 16:57:55 +0100

    Fixed status message not being set

--- src/setFor.js
+++ src/setFor.js
@@ -1,5 +1,6 @@
 let setFor = (response, status) => {
-  return response.status(status).send(require('./status')[status]);
+  response.statusMessage = require('./status')[status];
+  response.status(status);
 };
 
 module.exports = setFor;
